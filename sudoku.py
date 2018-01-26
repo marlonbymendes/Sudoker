9155da14b81aa3e868055867f200de0b73c753ac
Marlon Mendes <marlonciriatico@gmail.com>
2018-01-26 21:04:40 -0200

    Refactoring sudoku class

--- sudoku.py
+++ sudoku.py
@@ -1,22 +1,27 @@
 from grid import Grid
 
 class Sudoku(Grid):
-    __dimension = 9
-    __valid_cells = [i for i in range(dimension + 1)]
-    __solved_valid_cells = set([i for i in range(1, dimension + 1)])
+    _dimension = 9
+    _valid_cells = [i for i in range(_dimension + 1)]
+    _quadrant_size = _dimension // 3
+    _solved_valid_cells = set([i for i in range(1, _dimension + 1)])
 
-
+        
     def __init__(self):
-        super().__init__(Sudoku.__dimension, Sudoku.__dimension, Sudoku.__valid_cells)
+        super().__init__(Sudoku._dimension, Sudoku._dimension, Sudoku._valid_cells)
+
+    @property
+    def dimension(self):
+        return Sudoku._dimension
 
     # Both columns and rows are considered "lines"
     def is_line_solved(self, line):
-        return set(line) == __solved_valid_cells
+        return set(line) == self._solved_valid_cells
 
     def is_solved(self):
-        for i in range(Sudoku.__dimension):
-            if !is_line_solved(self.get_row(i)):
+        for i in range(self.dimension):
+            if not is_line_solved(self.get_row(i)):
                 return False
-            if !is_line_solved(self.get_column(i)):
+            if not is_line_solved(self.get_column(i)):
                 return False
         return True
