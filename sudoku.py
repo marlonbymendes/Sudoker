036017852525a87a50930830d49cd3bcfb04f47f
Marlon Mendes <marlonciriatico@gmail.com>
2018-02-03 00:37:24 -0200

    Completing function for testing wheter sudoku is solved or not

--- sudoku.py
+++ sudoku.py
@@ -5,10 +5,13 @@ class Sudoku(Grid):
     _valid_cells = [i for i in range(_dimension + 1)]
     _quadrant_size = _dimension // 3
     _solved_valid_cells = set([i for i in range(1, _dimension + 1)])
-
+    _quadrant_dimension = 3
         
-    def __init__(self):
-        super().__init__(Sudoku._dimension, Sudoku._dimension, Sudoku._valid_cells)
+    def __init__(self, table=None):
+        if table is None:
+            super().__init__(Sudoku._dimension, Sudoku._dimension, Sudoku._valid_cells)
+        else:
+            super().__init__(table=table)
 
     @property
     def dimension(self):
@@ -20,8 +23,23 @@ class Sudoku(Grid):
 
     def is_solved(self):
         for i in range(self.dimension):
-            if not is_line_solved(self.get_row(i)):
+            if not self.is_line_solved(self.get_row(i)):
                 return False
-            if not is_line_solved(self.get_column(i)):
+            if not self.is_line_solved(self.get_column(i)):
                 return False
+        for i in range(Sudoku._quadrant_dimension):
+            for j in range(Sudoku._quadrant_dimension):
+                quadrant = self.get_quadrant_as_list(i, j)
+                if not self.is_line_solved(quadrant):
+                    return False
         return True
+
+    def get_quadrant_as_list(self, row, column):
+        i_start = row * 3
+        j_start = column * 3
+        quadrant = []
+        
+        for i in range(i_start, i_start + 3):
+            for j in range(j_start, j_start + 3):
+                quadrant.append(self.get_cell(i, j))
+        return quadrant
--- test_sudoku.py
+++ test_sudoku.py
@@ -2,7 +2,39 @@ from sudoku import Sudoku
 
 class SudokuConstants:
     dimension = 9
+    quadrant_size = 3
     valid_cells = [i for i in range(dimension + 1)]
+    unsolved_sudoku = [[1, 2, 3, 1, 2, 3, 1, 2, 3],
+		       [4, 5, 6, 4, 5, 6, 4, 5, 6],
+		       [7, 8, 9, 7, 8, 9, 7, 8, 9],
+		       [1, 2, 3, 1, 2, 3, 1, 2, 3],
+		       [4, 5, 6, 4, 5, 6, 4, 5, 6],
+		       [7, 8, 9, 7, 8, 9, 7, 8, 9],
+		       [1, 2, 3, 1, 2, 3, 1, 2, 3],
+		       [4, 5, 6, 4, 5, 6, 4, 5, 6],
+		       [7, 8, 9, 7, 8, 9, 7, 8, 9]]
+
+    solved_sudoku = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
+                     [6, 7, 2, 1, 9, 5, 3, 4, 8],
+                     [1, 9, 8, 3, 4, 2, 5, 6, 7],
+                     [8, 5, 9, 7, 6, 1, 4, 2, 3],
+                     [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                     [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                     [9, 6, 1, 5, 3, 7, 2, 8, 4],
+                     [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                     [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+
+    unsolved_sudoku2 = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
+                        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+                        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+                        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+                        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+                        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                        [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+
+
 
 sConstants = SudokuConstants()
 
@@ -34,3 +66,26 @@ def test_set_cell_outside_grid():
     except IndexError:
         pass
 
+
+def test_get_valid_qudrant():
+    s = Sudoku(sConstants.unsolved_sudoku)
+    expected = [i for i in range(1, 10)]
+    for i in range(sConstants.quadrant_size):
+        for j in range(sConstants.quadrant_size):
+            quadrant = s.get_quadrant_as_list(i, j)
+            assert quadrant == expected
+
+def test_get_invalid_quadrant():
+    s = Sudoku(sConstants.unsolved_sudoku)
+    try:
+        quadrant = s.get_quadrant_as_list(sConstants.quadrant_size, sConstants.quadrant_size)
+    except IndexError:
+        pass
+
+def test_solved_sudoku():
+    s = Sudoku(sConstants.solved_sudoku)
+    assert s.is_solved()
+
+def test_unsolved_sudoku():
+    s = Sudoku(sConstants.unsolved_sudoku2)
+    assert not s.is_solved()
